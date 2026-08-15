d22b9352c5943368ad9da59848dfb94350757400
gmis-student <anjani.djohan@gmis.sch.id>
2026-08-15 17:54:44 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,3 @@
 function code() {
-    document.getElementById('links').innerHTML = '<a href="indexen.html">';
+    document.getElementById('links').innerHTML = '\[ <a href="indexen.html">> Home</a> | <a href="about.html">> About</a> | <a href="text.html">> Information Booklet</a> | <a href="game.html">> Game</a> | <a href="ppt.html">> Powerpoint</a> | <a href="poster.html">> Posters</a> \]';
 };
